2c605359d905e9708d9d7fa4bb5d0d5ed6723eb9
ryadyl <50424470+ryadyl@users.noreply.github.com>
2019-09-22 19:06:44 -0400

    Update octo.js

--- octo.js
+++ octo.js
@@ -1,8 +1,10 @@
 onmessage=function(e){console.log(e)};
-debugger;
 console.log('hi there!');
 console.log(self);
 console.log(self.clients);
 var g=new BroadcastChannel('g');
 g.postMessage('windowl');
 console.log(clients.claim);
+setTimeout(function() {
+    var p;
+}, (600*1000));
